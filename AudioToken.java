726e2e6badf5f5469675f63d0e775529ea1e0277
senjka4 <senjka4@users.noreply.github.com>
2018-10-27 16:05:49 +0300

    Добавил выбор скачиваемых песен

--- AudioToken.java
+++ AudioToken.java
@@ -1,14 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.lang.reflect.ParameterizedType;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringJoiner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class AudioToken {
     public static void main(String[] args) throws Exception{
@@ -18,31 +12,26 @@ public class AudioToken {
         URL url = new URL("https://android.clients.google.com/c2dm/register3");
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
         connection.setRequestMethod("POST");
-        String sj = "X-subtype=54740537194&X-X-subscription=54740537194&X-X-subtype=54740537194&X-app_ver=414&X-kid=%7CID%7C1%7C&X-osv=22&X-sig=hFPgsFXRSlTbShE30_-1ip5gDphq_xQiJYKppnD0ZEs4aZ5eRERU4HMEMCBPGAo6a75gxAWa2s3Xvz5_owUhaUSxiXHRTYzzc8PVyJZocrV_mdPDfOdGdNmlCAyZ_cq9oAXwNp-xxPFiwqGfbNc4JyhCMbbLkQ35NOwqBNRUdFpYpDu9dv9Z0XzhMR3-tRZ6akTJRj8Fm2agKvvxZwMt35xvzliUc5Z_dRph04i8qQ78yCpt12AkFJLUMxy47S-l4JOllcaIElZX9KHUXDNRWAcHkvmZbuXcNI3eDeSgquUpXqXwcy1_RQnuASAyZHZWfTwgJUybcNXXQ-4iFT1-Dg&X-cliv=iid-9452000&X-gmsv=11951270&X-pub2=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwg63eexVyrdV66YiH3R4a3De2_d3QwBsulptEenQ6pYzL3C1DP1t2Pt4VGC9ZmBXA1qUAe06i6kwViQzw2YkTtUGdbShsKzVh6aHPP0cpsDaMb8JYwVinuZWgYnrUBCOde-IJ0Y-f0Xer3gjDJQm-UZTW9PBvYCAdBty9BV73mQsoWkkn-yqlO8u9vch1kjfV8iF1GDS9mkiG1tCZcE4ZTUNMzasQ2WSuXXuyWOeGa0hIOfJe6DSIkzzrvrkQVErM5XQwjSJ5uQF0gcgfO_JFDrOwb8qN4xINZ1J1I-pGz0Lvev-ii3I5ERLnWqQThDRskk8oUoDnVho5ikbzo1rAQIDAQAB&X-X-kid=%7CID%7C1%7C&X-appid=fFcI2QYSt_k&X-scope=id" + uid + "&X-subscription=54740537194&X-app_ver_name=43&app=com.perm.kate&sender=54740537194&device=3744286266973960032&cert=ca7036ce4c5abe56b9f4439ea275171ceb0d35a4&app_ver=414&info=83nHWh1ZbN8UsMmGZHNgy8SlZbNx-hU&gcm_ver=11951270";
+        String sj = "X-subtype=54740537194&X-X-subscription=54740537194&X-X-subtype=54740537194&X-app_ver=431&X-kid=%7CID%7C2%7C&X-osv=22&X-sig=V0K0RL_JPZgbLTx3N6329klqHFx-Ih8Tm_Lv8JYmpS07fFkqGa-WHfqFUgH2kL7sWW9Qb-nyKwlS0HsW4qj_pJYnrErdQ3-6spEQIQ_yTdVg328op3tbPLicLIkIFtF3OJV_Re6AEf87aiLUC6qbg0lYZrQ2R6EigSc8EMrcDE2VDLk-OLEcDY70rJZlDBuHT0nHATvKJuY0fcVZiBJQTE5OZHRw07dpzgJ4Is8KSasGK0mE4kdBd-2APU2aFJWe-mtup-fM85mS5B9uJSBr0NwmW73EDarD_qBUp5qzF_J6MUQJ5E78tWt3Ntuajmj4nPk4iL8x5QrqJCPm1InBbw&X-cliv=iid-9452000&X-gmsv=11975236&X-pub2=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxzYzGXl3cum_2eQzOSPPMbL4wREqTJ4kbcrrPuzK49iU5eylb84CJrhS_8f7yqPHHAaIRCakJlIeOk_xV4HQs9qgenIGThphG87nKl3cefra-LfXIVs0dPWx_k7lJnM5_JfWxZisMU-FO448ZohRhffQN_ibnH58zLuDapVqOrW6rii30ABKJitU6H9FmXy7eSv3LmFsatsu1uVnSxAcRhhvW5G8peDprQ0Fbl1-XN0F9LHZMMnXi2drCKvZOeelsnfLmLvGeURTIApOGTF2ml9-5iBYd3NVa8heH6WpbtacdMcubjeXxbMgdheMvakgJWlQg2Y_LByvhTpV_iK_0QIDAQAB&X-X-kid=%7CID%7C2%7C&X-appid=egc2JXpLa7M&X-scope=id217132304&X-subscription=54740537194&X-app_ver_name=48.1&app=com.perm.kate&sender=54740537194&device=4018038774292298290&cert=ca7036ce4c5abe56b9f4439ea275171ceb0d35a4&app_ver=431&info=w815UAJ94VYfsMmGZHNgy8Tl9t_1DhY&gcm_ver=11975236";
         byte[] out = sj.getBytes(StandardCharsets.UTF_8);
         int length = out.length;
-        connection.setRequestProperty("User-Agent", "Android-GCM/1.5 (vbox86p LMY47D)");
+        connection.setRequestProperty("User-Agent", "Android-GCM/1.5 (m0 LMY48Y)");
         connection.addRequestProperty("Gcm-cert", "ca7036ce4c5abe56b9f4439ea275171ceb0d35a4");
-        connection.addRequestProperty("Gcm-ver", "11951270");
         connection.addRequestProperty("app", "com.perm.kate");
-        connection.addRequestProperty("Authorization", "AidLogin 3744286266973960032:6946597583564230085");
+        connection.addRequestProperty("Gcm-ver", "11975236");
+        connection.addRequestProperty("Authorization", "AidLogin 4018038774292298290:8687452178090903598");
         connection.setFixedLengthStreamingMode(length);
         connection.setDoOutput(true);
         connection.connect();
         try(OutputStream os = connection.getOutputStream()) {
             os.write(out);
         }
-        int code = connection.getResponseCode();
-        System.out.println(code);
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String  body = "", output;
         while ((output = br.readLine()) != null) {
-            body += output;
+            body = body.concat(output);
         }
-        Pattern pat = Pattern.compile(":");
-        Matcher mat = pat.matcher(body);
-
-
+        System.out.println(body);
         return body.replaceAll("token=\\|ID\\|", "").substring(3).replaceAll("\n", "").replaceAll(" ","");
     }
 }
--- MainClass.java
+++ MainClass.java
@@ -1,73 +1,205 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.*;
 import java.net.*;
 import java.net.HttpURLConnection;
+import java.text.DecimalFormat;
+
 public class MainClass {
-    public static String USER_AGENT = "KateMobileAndroid/43-414 (Android 5.1; SDK 22; x86; unknown ANDROID_LOLIPOPKA; en)", MID = "";
+    private static String MID = "";
+    private static BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws Exception{
-        String access_token = login(), audioJson= "";
-        do{
-            access_token = updateToken(access_token);
-            System.out.println("New Access Token is: " + access_token);
-            System.out.println("Retry.............");
-            audioJson = get("https://api.vk.com/method/audio.get?access_token=" + access_token + "&owner_id=" + MID + "&v=5.68");
-        }while (audioJson.contains("confirmation"));
-        System.out.println("Json from Audio is: " + audioJson);
+        try {
+            String access_token = login(), audioJson;
+            do {
+                access_token = updateToken(access_token);
+                System.out.println("New Access Token is: " + access_token);
+                audioJson = get("https://api.vk.com/method/audio.get?access_token=" + access_token + "&owner_id=" + MID + "&v=5.68");
+            } while (audioJson.contains("confirmation"));
+            processAudioJson(audioJson);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            r.close();
+        }
+
     }
-    public static String updateToken(String oldToken) throws Exception{
+
+    private static void processAudioJson(String audioJson) throws Exception{
+        JSONObject jsonObject = new JSONObject(audioJson);
+        JSONObject response = jsonObject.getJSONObject("response");
+        JSONArray items = response.getJSONArray("items");
+        int index = 1;
+        for(Object itemObject : items){
+            JSONObject item = (JSONObject) itemObject;
+            String title = item.getString("title");
+            String artist = item.getString("artist");
+            int duration = item.getInt("duration");
+            int minutes = duration /60, seconds = Integer.parseInt(new DecimalFormat("00").format(duration % 60));
+            String sec;
+            if(seconds < 10){
+                sec = "0" + String.valueOf(seconds);
+            }else{
+                sec = String.valueOf(seconds);
+            }
+            String url = item.getString("url");
+            if(!url.equals("")) {
+                print(index + ": " + minutes + ":" + sec + " Url - " + url);
+                index++;
+            }
+        }
+        print("Select audio number to download. \n To download all - type \"all\". \n To download specific audio - type \"spec\" Example - 1,2,3,4... \n To download from some audio number to another - type \"range\"");
+        String input = r.readLine();
+        switch (input){
+            case "all":
+                print("Ok, i will download all.");
+                print("To download to specific folder type it's absolute path.\nIf you want to download in this folder type folder name");
+                String folderPath = r.readLine();
+                File musicFolder;
+                try{
+                    musicFolder = new File(folderPath);
+                    if(musicFolder.mkdir()) {
+                        for (Object musicElementObject : items) {
+                            JSONObject musicElement = (JSONObject) musicElementObject;
+                            if (!musicElement.getString("url").equals("")) {
+                                String title = musicElement.getString("title"),
+                                        artist = musicElement.getString("artist"),
+                                        url = musicElement.getString("url");
+                                System.out.println("Downloading - " + title + " - " + artist);
+                                File musicFile = new File(musicFolder.getAbsolutePath() + "/" + title + " - " + artist + ".mp3");
+                                if (!musicFile.exists()) {
+                                    if(!musicFile.createNewFile()){
+                                        return;
+                                    }
+                                }
+                                writeToFile(url, musicFile);
+                            }
+                        }
+                    }
+                }catch (Exception e){
+                    print("Failed to find folder!\nExiting program!");
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+
+                break;
+            case "spec":
+
+                break;
+            case "range":
+
+                break;
+            default:
+                print("Something is wrong. \n Bye!");
+                break;
+        }
+    }
+
+    private static void print(String print){
+        System.out.println(print);
+    }
+
+    private static String updateToken(String oldToken) throws Exception{
         String audio_token = AudioToken.getToken(MainClass.MID);
         return new JSONObject(get("https://api.vk.com/method/auth.refreshToken?access_token=" + oldToken  + "&receipt=" + audio_token + "&v=5.68")).getJSONObject("response").getString("token");
     }
-    public static String get(String uri)throws Exception{
-        URL url = new URL(uri);
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("User-Agent", "KateMobileAndroid/43-414 (Android 5.1; SDK 22; x86; unknown ANDROID_LOLIPOPKA; en)");
-        //connection.setRequestProperty("Accept-Encoding", "gzip");
-        //connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-        connection.connect();
-        CookieHandler.setDefault( new CookieManager( null, CookiePolicy.ACCEPT_ALL ) );
-        int code = connection.getResponseCode();
-        BufferedReader br = null;
-        if (200 <= connection.getResponseCode() && connection.getResponseCode() <= 299) {
-            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        } else {
-            br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-        }
-        String out = "", output;
-        while ((output = br.readLine()) != null) {
-            out += output;
-        }
-        return out;
-    }
-    public static String login() throws Exception{
+
+    private static String login() throws Exception{
         System.out.println("Enter login: ");
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         String login = r.readLine();
         System.out.println("Enter Password: ");
         String pass = r.readLine();
-        String loginUrl = "https://oauth.vk.com/token?grant_type=password&client_id=2685278&client_secret=lxhD8OD7dMsqtXIm5IUY&username=" + login + "&password=" + pass + "&v=5.68&scope=notify%2Cfriends%2Cphotos%2Caudio%2Cvideo%2Cdocs%2Cstatus%2Cnotes%2Cpages%2Cwall%2Cgroups%2Cmessages%2Coffline%2Cnotifications";
+        String loginUrl = "https://oauth.vk.com/token?grant_type=password&client_id=2685278&client_secret=lxhD8OD7dMsqtXIm5IUY&username=" + URLEncoder.encode(login, "UTF-8") + "&password=" + URLEncoder.encode(pass, "UTF-8") + "&v=5.68&scope=notify%2Cfriends%2Cphotos%2Caudio%2Cvideo%2Cdocs%2Cstatus%2Cnotes%2Cpages%2Cwall%2Cgroups%2Cmessages%2Coffline%2Cnotifications";
         String resp = get(loginUrl);
-        System.out.println(resp);
-        String access_token = "";
+        String access_token;
         if(resp.contains("captcha")){
-            System.out.println("Captcha json is: " + resp);
-            System.out.println("Enter key");
-            String key = r.readLine();
-            String id = resp.split("sid=")[1].replaceAll("}", "").replaceAll("\"", "");
-            System.out.println("Captcha id is: " + id);
-            String body = get(loginUrl + "&captcha_sid=" + id + "&captcha_key=" + key);
-            System.out.println("After login token json is: " + body);
+            String body = verifyCaptcha(resp, loginUrl);
             JSONObject object = new JSONObject(body);
             access_token = object.getString("access_token");
             MainClass.MID = String.valueOf(object.getInt("user_id"));
         }else{
+            System.out.println(resp);
             JSONObject object = new JSONObject(resp);
             access_token = object.getString("access_token");
             MainClass.MID = String.valueOf(object.getInt("user_id"));
         }
-        r.close();
+        System.out.println("After login token is: " + access_token);
         return access_token;
     }
 
+    private static String verifyCaptcha(String resp, String loginUrl) throws Exception{
+        String key , id, toReturn;
+        do {
+            System.out.println("Captcha url: " + new JSONObject(resp).getString("captcha_img"));
+            System.out.println("Enter key");
+            key = r.readLine();
+            id = new JSONObject(resp).getString("captcha_sid");
+            toReturn = get(loginUrl + "&captcha_sid=" + id + "&captcha_key=" + key);
+        }while(toReturn.contains("captcha"));
+        return toReturn;
+    }
+    private static String get(String url){
+        URL obj;
+        HttpURLConnection con = null;
+        try {
+            obj = new URL(url);
+            con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "KateMobileAndroid/50.2 lite-440 (Android 7.0; SDK 24; arm64-v8a; samsung SAMSUNG-SM-G935A; ru)");
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return response.toString();
+        }catch (Exception e){
+            try {
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getErrorStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                return response.toString();
+            }catch (Exception ignored){
+                return "";
+            }
+        }
+
+    }
+    private static void writeToFile(String httpUrl, File file) throws Exception{
+        URL url = new URL(httpUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        InputStream stream = connection.getInputStream();
+        FileOutputStream outputStream = new FileOutputStream(file);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int read;
+        try {
+            while ((read = stream.read(buffer, 0, buffer.length)) != -1) {
+                baos.write(buffer, 0, read);
+            }
+        }catch (Exception ignored){
+
+        }
+        baos.flush();
+        byte[] result = baos.toByteArray();
+        for (byte el : result){
+            outputStream.write(el);
+        }
+        outputStream.flush();
+        outputStream.close();
+        stream.close();
+        baos.close();
+    }
 }
